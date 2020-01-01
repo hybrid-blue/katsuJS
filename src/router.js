480574a1481342e31b241712bc28a98da1402e4c
hybrid_blue <aaronifill@gmail.com>
2020-01-01 17:33:42 +0000

    Add support for the 'set' api and fix default route on load.

--- src/router.js
+++ src/router.js
@@ -2,28 +2,111 @@ import { setPage, getPage } from './app/utilis';
 
 export default class Router{
 
-  constructor(pageRoot, viewTarget, defaultPage = "", routes){
+  constructor(pageRoot, viewTarget, defaultPage = "", routes, onRouteChange){
     this.pageRoot = pageRoot;
     this.viewTarget = viewTarget;
     this.defaultPage = defaultPage;
     this.routes = routes;
+    this.onRouteChange = onRouteChange;
+    this.selectedRoute;
+    this.selectedRouteObj;
+    this.obj;
   }
 
-  renderPage(selector, afterAppInit){
-     const render = async(pageRoot, viewTarget) => {
-      var res = await getPage(selector, this.pageRoot);
+
+  findRoute(selector, thisLevelRoutes, level = 0){
+    var template;
+    const routes = selector.split('/');
+    for(let thisRoute of thisLevelRoutes){
+
+        let route = routes[level];
+
+        if(thisRoute[route]){
+
+          if(thisRoute[route].subRoute && level < routes.length - 1){
+            level++
+            this.findRoute(selector, thisRoute[route].subRoute, level);
+          }else{
+
+
+            if(thisRoute[route].template && !window.blade.temp.template){
+              template = thisRoute[route].template
+              this.selectedRoute = template;
+            }else{
+              this.selectedRoute = window.blade.temp.template;
+            }
+
+            this.selectedRouteObj = thisRoute[route];
+            this.obj = thisRoute;
+
+            break;
+          }
+        }
+
+    }
+
+  }
+
+  renderLoading(){
+    document.querySelector(this.viewTarget).innerHTML = `<h2 id="blade-loading">Loading<span></span></h2>`;
+  }
+
+
+  setRouteProps(route, thisObj, thisRoute){
+
+    this.onRouteChange();
+
+    var template, data;
+
+    if(Object.entries(window.blade.props.template).length === 0){
+      template = this.selectedRouteObj.template || null;
+      window.blade.props.template = template || null;
+    }else{
+      template = window.blade.props.template;
+    }
+
+    if(typeof(thisObj) === 'object'){
+      route = Object.keys(route)[0];
+      data = this.selectedRouteObj.data || window.blade.props.data;
+    }else{
+      route = thisRoute;
+      data = {} || window.blade.props.data;
+    }
+    return {template, data, route}
+
+  }
+
+
+
+  renderPage(selector, afterAppInit = () => {}){
+
+    this.renderLoading();
+
+    var thisLink = document.querySelector(`[data-blade-route='${selector.replace(/\//g,'-', '-')}']`);
+    var type = this.findRoute(selector, this.routes);
+
+    var thisLinkHref = thisLink.getAttribute('href');
+    var template = this.selectedRoute || null;
+    var data = this.selectedRouteObj.data || {};
+
+    const render = async(template) => {
+      var props = await this.setRouteProps(this.obj, this.obj, selector);
+      window.blade.temp.template = props.template || null;
+
+      var res = await getPage(`/${selector}`, this.pageRoot, template);
       const statusCode = await res.status;
       if(statusCode === 404){
         let content = '<h1>404</h1><h2>Page was not found</h2>'
-        await setPage(content, this.viewTarget);
+        await setPage(content, this.viewTarget, props.data);
         await afterAppInit()
       }else{
         let content = await res.text();
-        await setPage(content, this.viewTarget);
+        await setPage(content, this.viewTarget, props.data);
         await afterAppInit()
       }
     }
-    render();
+
+    render(template);
   }
 
   defaultRoute(afterAppInit, onRouteInit){
@@ -59,7 +142,11 @@ export default class Router{
 
   handleBrowserNavigation(routes, onRouteChange){
     window.addEventListener('popstate', (event) => {
-      onRouteChange()
+      console.log('Pressed Navigation');
+      window.blade.props = {};
+      window.blade.temp = {};
+      this.selectedRoute = null;
+      this.selectedRouteObj = null;
       let page = event.path[0].location.pathname;
       let selector = page.substr(1, page.length).toLowerCase();
       this.renderPage(selector);
